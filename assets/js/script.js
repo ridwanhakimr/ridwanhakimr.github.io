c781901b827a514a5c0c47cee198b5ce3fcd83f1
911AGENT <ridwanhakimrmdhn@gmail.com>
2025-03-11 23:21:25 +0700

    perubahan tampilan

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function() {
+            document.querySelector(".active")?.classList.remove("active");
+            this.classList.add("active");
+        });
+    });
+});
