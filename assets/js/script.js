393220c00780f974b5c9310273e3be4a9c596e09
911AGENT <ridwanhakimrmdhn@gmail.com>
2025-03-14 13:05:22 +0700

    menambahkan kontak

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,21 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Mencegah form submit default
+
+    // Ambil nilai input
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    // Validasi sederhana
+    if (name && email && subject && message) {
+      alert('Pesan berhasil dikirim!');
+      document.getElementById('contactForm').reset(); // Reset form
+    } else {
+      alert('Harap isi semua field!');
+    }
+  });
